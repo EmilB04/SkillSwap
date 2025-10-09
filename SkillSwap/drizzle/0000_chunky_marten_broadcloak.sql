b875234d1cd36308f3d64adef4bfa957ecf81ea1
Andolaus <andolaus@gmail.com>
2025-10-09 20:42:28 +0200

    Had to migrate again due to a lost table

--- SkillSwap/drizzle/0000_chunky_marten_broadcloak.sql
+++ SkillSwap/drizzle/0000_chunky_marten_broadcloak.sql
@@ -1,3 +1,11 @@
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`email` text NOT NULL,
+	`role` text DEFAULT 'user' NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);--> statement-breakpoint
 CREATE TABLE `profile_details` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`user_id` integer NOT NULL,
--- SkillSwap/src/db/schema/users.ts
+++ SkillSwap/src/db/schema/users.ts
@@ -1,5 +1,4 @@
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
-import { timestamps } from "./columns.helpers";
 
 // https://orm.drizzle.team/docs/sql-schema-declaration 
 
