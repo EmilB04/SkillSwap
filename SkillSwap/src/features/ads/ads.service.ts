8679f28262d0e63085932a4807ca5196f94445c0
Andolaus <andolaus@gmail.com>
2025-10-24 17:09:51 +0200

    Created ads.service

--- SkillSwap/src/features/ads/ads.service.ts
+++ SkillSwap/src/features/ads/ads.service.ts
@@ -10,3 +10,35 @@ export interface AdsService {
     update(id: string, data: Partial<InsertAd>): Promise<Result<Ad>>;
 }
 
+// Factory function to create an AdsService instance
+export function createAdsService(repository: AdsRepository): AdsService {
+    return {
+        async list(params = {}) {
+            const repositoryResults = await repository.findMany(params);
+            if (!repositoryResults.success) return repositoryResults;
+
+            const { data } = repositoryResults;
+            return { success: true, data: data ?? [] };
+        },
+
+        // Get a single ad by its ID
+        async getById(id: string) {
+            const repositoryResults = await repository.findById(id);
+            if (!repositoryResults.success) return repositoryResults;
+            
+            return { success: true, data: repositoryResults.data}; 
+        },
+
+        // Create a new ad
+        async create(data: InsertAd) {
+            return repository.create(data);
+        },
+
+        // Update an existing ad
+        async update(id: string, data: Partial<InsertAd>) {
+            return repository.update(id, data);
+        }
+    };
+}
+
+export const adsService = createAdsService(adsRepository);
