cb5ed0e213ad25a6929fb5be71155ad3d0ba672c
Emil Berglund <emil.berglund@live.no>
2025-11-05 01:44:49 +0100

    Feat: New and improved design for Explore with components.

---
+++ SkillSwap/src/app/components/explore/index.ts
@@ -0,0 +1,5 @@
+export { default as ExploreHeader } from './ExploreHeader';
+export { default as FilterSection } from './FilterSection';
+export { default as ResultsSummary } from './ResultsSummary';
+export { default as JobGrid } from './JobGrid';
+export { default as EmptyState } from './EmptyState';
