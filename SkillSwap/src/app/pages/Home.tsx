a0900657d2903f1681ee804fa9c8563f99cd5f89
Emil Berglund <emil.berglund@live.no>
2025-12-01 13:34:52 +0100

    refactor: Remove TestUserToggle component from Home page

--- SkillSwap/src/app/pages/Home.tsx
+++ SkillSwap/src/app/pages/Home.tsx
@@ -7,7 +7,6 @@ import { HowItWorks } from "../components/home/HowItWorks";
 import { FeaturedOpportunities } from "../components/home/FeaturedOpportunities";
 import { Hero } from "../components/home/Hero";
 import ScrollToTop from "../components/ScrollToTop";
-import TestUserToggle from "../components/TestUserToggle";
 
 // TODO: Remove this import when integrating with backend
 import { mockJobs } from "../../types/job";
@@ -99,7 +98,6 @@ export function Home({ ctx }: RequestInfo) {
         )}
       </div>
       <ScrollToTop />
-      <TestUserToggle />
     </PageLayout>
   );
 }
